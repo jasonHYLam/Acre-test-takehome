66ad0875c64651a78bd763e75e76a1f022951aad
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-31 22:04:38 +0100

    Add title assertion

--- tests/affordability-test.spec.ts
+++ tests/affordability-test.spec.ts
@@ -1,9 +1,11 @@
 // @ts-check
 
 const CALCULATOR_URL = "https://intermediaries.hsbc.co.uk/calculator/";
+const TITLE = "Affordability calculator | HSBC for Intermediaries";
 
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto(CALCULATOR_URL);
+  await expect(page).toHaveTitle(TITLE);
 });
