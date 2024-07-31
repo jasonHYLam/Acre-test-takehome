a85595141ade1fb0875426d6fc5f0ddae4e1d69e
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-31 22:35:30 +0100

    Add test for no user details and change test name

--- tests/affordability-test.spec.ts
+++ tests/affordability-test.spec.ts
@@ -6,7 +6,19 @@ const TITLE = "Affordability Calculator | HSBC UK for Intermediaries";
 
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test("not filling in user details results in no lending and validation errors", async ({
+  page,
+}) => {
   await page.goto(CALCULATOR_URL);
   await expect(page).toHaveTitle(TITLE);
+
+  await page.getByText("view", { exact: true }).click();
+
+  await expect(page.getByText("The Any element of foreign")).toBeVisible();
+  await expect(page.getByText("The Property Value field")).toBeVisible();
+  await expect(page.locator("#lendingBasedOnProperty")).toContainText("0");
+  await expect(page.locator("#resultantLTV")).toContainText("0");
+  await expect(page.locator("#lendingBasedOnAffordability")).toContainText(
+    "NOT AVAILABLE"
+  );
 });
