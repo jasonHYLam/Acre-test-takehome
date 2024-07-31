a60006c6066a7889882c52281de333e8402b9079
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-07-31 23:39:54 +0100

    Create test for providing property value but not income

--- tests/affordability-test.spec.ts
+++ tests/affordability-test.spec.ts
@@ -22,3 +22,26 @@ test("not filling in user details results in no lending and validation errors", 
     "NOT AVAILABLE"
   );
 });
+
+// Test providing mortgage details but no income results in no lending
+test("providing property value but no income results in no lending", async ({
+  page,
+}) => {
+  await page.goto(CALCULATOR_URL);
+  await expect(page).toHaveTitle(TITLE);
+
+  await page.getByText("1 Mortgage Details").click();
+  await page.getByLabel("Property Value:").click();
+  await page.getByLabel("Property Value:").fill("1000000");
+  await page.getByLabel("Property Value:").press("Enter");
+  await page.getByRole("button", { name: "Next step" }).click();
+  await page.getByText("view", { exact: true }).click();
+  await expect(page.locator("#result-errors")).toBeVisible();
+  await expect(page.locator("#lendingBasedOnProperty")).toContainText("0");
+  await expect(page.locator("#resultantLTV")).toContainText("0");
+  await expect(page.locator("#lendingBasedOnAffordability")).toContainText(
+    "NOT AVAILABLE"
+  );
+});
+
+// Test providing income but no mortgage details results in no lending
