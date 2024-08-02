655ee8dab9361affc63e52f0f716794cc4c88de3
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-08-02 20:57:43 +0100

    Create checkValidIncomeDetailsForLending helper
    This checks whether lending is possible, based on whether certain income
    details are provided. This additionally depends on whether the mortgage
    is sole or joint.

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -1,6 +1,26 @@
 import { ProvidedDetails, AllIncomeDetails } from "./types";
 
-export async function checkValidIncomeDetailsForLendingForSoleMortgage(
+export async function checkValidIncomeDetailsForLending(
+  providedDetails: ProvidedDetails
+) {
+  if (providedDetails.mortgageDetails) {
+    const { jointMortgage } = providedDetails.mortgageDetails;
+
+    if (providedDetails.allIncomeDetails) {
+      if (jointMortgage === null || !jointMortgage) {
+        return checkValidIncomeDetailsForLendingForSoleMortgage(
+          providedDetails.allIncomeDetails
+        );
+      } else {
+        return checkValidIncomeDetailsForLendingForJointMortgage(
+          providedDetails.allIncomeDetails
+        );
+      }
+    }
+  }
+}
+
+async function checkValidIncomeDetailsForLendingForSoleMortgage(
   allIncomeDetails: AllIncomeDetails
 ) {
   const { applicant1IncomeDetails } = allIncomeDetails;
@@ -12,7 +32,7 @@ export async function checkValidIncomeDetailsForLendingForSoleMortgage(
   );
 }
 
-export async function checkValidIncomeDetailsForLendingForJointMortgage(
+async function checkValidIncomeDetailsForLendingForJointMortgage(
   allIncomeDetails: AllIncomeDetails
 ) {
   const { applicant1IncomeDetails, applicant2IncomeDetails } = allIncomeDetails;
@@ -20,6 +40,9 @@ export async function checkValidIncomeDetailsForLendingForJointMortgage(
   return (
     applicant1IncomeDetails &&
     applicant1IncomeDetails.grossIncome !== null &&
-    applicant1IncomeDetails !== null
+    applicant1IncomeDetails !== null &&
+    applicant2IncomeDetails &&
+    applicant2IncomeDetails.grossIncome !== null &&
+    applicant2IncomeDetails !== null
   );
 }
