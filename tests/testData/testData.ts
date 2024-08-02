706f18de8d6baf5b6e5b9c7f5632e261cfcc9280
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-08-02 17:59:36 +0100

    Update testData to use new interfaces

--- tests/testData/testData.ts
+++ tests/testData/testData.ts
@@ -19,47 +19,63 @@ export const testData: ProvidedDetails[] = [
       maxLTV: 0.85,
       mortgageTerm: 5,
     },
-    incomeDetails: {
-      grossIncome: 100000,
-      foreignCurrency: false,
+    allIncomeDetails: {
+      applicant1IncomeDetails: {
+        grossIncome: 100000,
+        foreignCurrency: false,
+      },
     },
   },
 
   // Input data for scenario where lending is not given, as expenditure is too great.
   {
     mortgageDetails: { propertyValue: 1000000 },
-    incomeDetails: { grossIncome: 10000, foreignCurrency: false },
+    allIncomeDetails: {
+      applicant1IncomeDetails: { grossIncome: 10000, foreignCurrency: false },
+    },
   },
 
   // Input data for scenario where lending is not given, as expenditure is too great.
   {
     mortgageDetails: { propertyValue: 1000000 },
-    incomeDetails: { grossIncome: 100000 },
-    expenditureDetails: { monthlyBTLOutgoings: 3700 },
+    allIncomeDetails: {
+      applicant1IncomeDetails: { grossIncome: 100000 },
+    },
+    allExpenditureDetails: {
+      applicant1ExpenditureDetails: { monthlyBTLOutgoings: 3700 },
+    },
   },
 
   // Input data for scenario where lending is not given, as foreignCurrency is not provided.
   {
     mortgageDetails: { propertyValue: 1000000 },
-    incomeDetails: { grossIncome: 100000 },
+    allIncomeDetails: {
+      applicant1IncomeDetails: { grossIncome: 100000 },
+    },
   },
 
   // Input data for scenario where lending is not given, as propertyValue is 0.
   {
     mortgageDetails: { propertyValue: 0 },
-    incomeDetails: { grossIncome: 0, foreignCurrency: false },
+    allIncomeDetails: {
+      applicant1IncomeDetails: { grossIncome: 0, foreignCurrency: false },
+    },
   },
 
   // Input data for scenario where lending is not given, as propertyValue is a negative integer.
   {
     mortgageDetails: { propertyValue: -1 },
-    incomeDetails: { grossIncome: 0, foreignCurrency: false },
+    allIncomeDetails: {
+      applicant1IncomeDetails: { grossIncome: 0, foreignCurrency: false },
+    },
   },
 
   // Input data for scenario where lending is not given, as propertyValue is below minimum property value.
   {
     mortgageDetails: { propertyValue: 1 },
-    incomeDetails: { grossIncome: 100000, foreignCurrency: false },
+    allIncomeDetails: {
+      applicant1IncomeDetails: { grossIncome: 100000, foreignCurrency: false },
+    },
   },
 
   // Input data for scenario where no details are provided.
