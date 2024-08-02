6243c8f46d08dc8dfc85f7a52d84c818716c7ddf
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-08-02 18:08:07 +0100

    Update handle incomeDetails in test

--- tests/affordability-test.spec.ts
+++ tests/affordability-test.spec.ts
@@ -35,31 +35,35 @@ testData.forEach((input, index) => {
       }
     }
 
-    if (input.incomeDetails) {
+    if (input.allIncomeDetails) {
       await page.getByText("2 Income").click();
-      if (
-        input.incomeDetails.grossIncome ||
-        input.incomeDetails.grossIncome === 0
-      ) {
-        await page.getByRole("spinbutton", { name: "Gross Income:" }).click();
-        await page
-          .getByRole("spinbutton", { name: "Gross Income:" })
-          .fill("100000");
-        await page
-          .getByRole("spinbutton", { name: "Gross Income:" })
-          .press("Enter");
-      }
+      const { applicant1IncomeDetails, applicant2IncomeDetails } =
+        input.allIncomeDetails;
+      if (applicant1IncomeDetails) {
+        if (
+          applicant1IncomeDetails.grossIncome ||
+          applicant1IncomeDetails.grossIncome === 0
+        ) {
+          await page.getByRole("spinbutton", { name: "Gross Income:" }).click();
+          await page
+            .getByRole("spinbutton", { name: "Gross Income:" })
+            .fill("100000");
+          await page
+            .getByRole("spinbutton", { name: "Gross Income:" })
+            .press("Enter");
+        }
 
-      if (
-        input.incomeDetails.foreignCurrency ||
-        input.incomeDetails.foreignCurrency === false
-      ) {
-        await page.getByRole("button", { name: "Nothing selected" }).click();
-        await page
-          .getByRole("menu")
-          .locator("a")
-          .filter({ hasText: "No" })
-          .click();
+        if (
+          applicant1IncomeDetails.foreignCurrency ||
+          applicant1IncomeDetails.foreignCurrency === false
+        ) {
+          await page.getByRole("button", { name: "Nothing selected" }).click();
+          await page
+            .getByRole("menu")
+            .locator("a")
+            .filter({ hasText: "No" })
+            .click();
+        }
       }
     }
 
