0c0d11ef7208cc29b233b696c89658c5acf6fca2
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-08-03 13:42:01 +0100

    Introduce all mortgageDetail properties to handleMortgageDetails

--- helpers/handleMortgageDetails.ts
+++ helpers/handleMortgageDetails.ts
@@ -1,16 +1,34 @@
 import { Page } from "@playwright/test";
 import { MortgageDetails } from "./types";
+import { clickAndEnterNumericalInput } from "./utils";
 
 export async function handleMortgageDetails(
   page: Page,
   mortgageDetails: MortgageDetails
 ) {
   await page.getByText("1 Mortgage Details").click();
-  if (mortgageDetails.propertyValue || mortgageDetails.propertyValue === 0) {
-    await page.getByLabel("Property Value:").click();
-    await page
-      .getByLabel("Property Value:")
-      .fill(mortgageDetails.propertyValue.toString());
-    await page.getByLabel("Property Value:").press("Enter");
+
+  const {
+    purchaserType,
+    jointMortgage,
+    maxLTV,
+    applicant1Age,
+    applicant2Age,
+    applicant1EmploymentStatus,
+    applicant2EmploymentStatus,
+    martialStatus,
+    dependantChildren,
+    dependantAdults,
+    depositAmount,
+    loanAmount,
+    propertyValue,
+    mortgageTerm,
+    assessOnInterestOnlyBasis,
+    propertyPostcode,
+  } = mortgageDetails;
+
+  if (propertyValue || propertyValue === 0) {
+    const propertyValueInput = page.getByLabel("Property Value:");
+    await clickAndEnterNumericalInput(propertyValueInput, propertyValue);
   }
 }
