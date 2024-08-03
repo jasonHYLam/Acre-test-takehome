da3d2692dc2352808e2b4ccfc48d14200e227f3a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-08-03 13:19:41 +0100

    Use handleIncomeDetailsForOneApplicant

--- helpers/handleIncomeDetails.ts
+++ helpers/handleIncomeDetails.ts
@@ -9,79 +9,10 @@ export async function handleIncomeDetails(
   await page.getByText("2 Income").click();
   const { applicant1IncomeDetails, applicant2IncomeDetails } = allIncomeDetails;
   if (applicant1IncomeDetails) {
-    const {
-      grossIncome,
-      foreignCurrency,
-      additionalIncome,
-      limitedCompanyNetProfits,
-      otherNonTaxableIncome,
-      existingBTLRentalIncome,
-    } = applicant1IncomeDetails;
-    if (grossIncome || grossIncome === 0) {
-      const grossIncomeInput = page.getByRole("spinbutton", {
-        name: "Gross Income:",
-      });
-      await clickAndEnterNumericalInput(grossIncomeInput, grossIncome);
-    }
-
-    //   await page.locator('#a1grossIncome').click();
-    // await page.locator('#a1grossIncome').fill('1');
-    // await page.locator('#a2grossIncome').click();
-    // await page.locator('#a2grossIncome').fill('2');
-    // await page.getByRole('spinbutton', { name: 'Gross Income:' }).click();
-
-    // TODO: This current only handles one applicant.
-    // ^ Modify such that either a single or both applicants are handled.
-    // TODO: Modify this to handle the other cases; currently only handles No; may require switch statement
-    //  await page.getByRole('button', { name: 'Nothing selected' }).click();
-    //  await page.getByRole('menu').locator('a').filter({ hasText: 'Yes - 30% Haircut to be' }).click();
-    if (foreignCurrency || foreignCurrency === false) {
-      await page.getByRole("button", { name: "Nothing selected" }).click();
-      await page
-        .getByRole("menu")
-        .locator("a")
-        .filter({ hasText: "No" })
-        .click();
-    }
-
-    if (additionalIncome || additionalIncome === 0) {
-      const additionalIncomeInput = page.getByRole("spinbutton", {
-        name: "Additional Income:",
-      });
-      await clickAndEnterNumericalInput(
-        additionalIncomeInput,
-        additionalIncome
-      );
-    }
-
-    if (limitedCompanyNetProfits || limitedCompanyNetProfits === 0) {
-      const limitedCompanyNetProfitsInput = page.getByRole("spinbutton", {
-        name: "Limited Company Net profits:",
-      });
-      await clickAndEnterNumericalInput(
-        limitedCompanyNetProfitsInput,
-        limitedCompanyNetProfits
-      );
-    }
-    if (otherNonTaxableIncome || otherNonTaxableIncome === 0) {
-      const otherNonTaxableIncomeInput = page.getByRole("spinbutton", {
-        name: "Other Non Taxable Income:",
-      });
-      await clickAndEnterNumericalInput(
-        otherNonTaxableIncomeInput,
-        otherNonTaxableIncome
-      );
-    }
-
-    if (existingBTLRentalIncome || existingBTLRentalIncome === 0) {
-      const existingBTLRentalIncomeInput = page.getByRole("spinbutton", {
-        name: "Existing BTL Rental Income:",
-      });
-      await clickAndEnterNumericalInput(
-        existingBTLRentalIncomeInput,
-        existingBTLRentalIncome
-      );
-    }
+    await handleIncomeDetailsForOneApplicant(page, applicant1IncomeDetails, 1);
+  }
+  if (applicant2IncomeDetails) {
+    await handleIncomeDetailsForOneApplicant(page, applicant2IncomeDetails, 2);
   }
 }
 
