7fa553a14f594717a3accbb1f6fab36c7e33f9b3
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-08-03 13:16:49 +0100

    Reduce code duplication when handling income details
    This is done by creating handleIncomeDetailsForOneApplicant which takes
    the income details and applicant number and page as arguments and
    performs the necessary input actions for the corresponding applicant.

--- helpers/handleIncomeDetails.ts
+++ helpers/handleIncomeDetails.ts
@@ -1,5 +1,5 @@
 import { Page } from "@playwright/test";
-import { AllIncomeDetails } from "./types";
+import { AllIncomeDetails, IncomeDetails } from "./types";
 import { clickAndEnterNumericalInput } from "./utils";
 
 export async function handleIncomeDetails(
@@ -23,6 +23,12 @@ export async function handleIncomeDetails(
       });
       await clickAndEnterNumericalInput(grossIncomeInput, grossIncome);
     }
+
+    //   await page.locator('#a1grossIncome').click();
+    // await page.locator('#a1grossIncome').fill('1');
+    // await page.locator('#a2grossIncome').click();
+    // await page.locator('#a2grossIncome').fill('2');
+    // await page.getByRole('spinbutton', { name: 'Gross Income:' }).click();
 
     // TODO: This current only handles one applicant.
     // ^ Modify such that either a single or both applicants are handled.
@@ -76,5 +82,68 @@ export async function handleIncomeDetails(
         existingBTLRentalIncome
       );
     }
+  }
+}
+
+async function handleIncomeDetailsForOneApplicant(
+  page: Page,
+  incomeDetails: IncomeDetails,
+  applicantNumber: number
+) {
+  const {
+    grossIncome,
+    foreignCurrency,
+    additionalIncome,
+    limitedCompanyNetProfits,
+    otherNonTaxableIncome,
+    existingBTLRentalIncome,
+  } = incomeDetails;
+  if (grossIncome || grossIncome === 0) {
+    const grossIncomeInput = page.locator(`#a${applicantNumber}grossIncome`);
+    await clickAndEnterNumericalInput(grossIncomeInput, grossIncome);
+  }
+
+  // TODO: Modify this to handle the other cases; currently only handles No; may require switch statement
+  //  await page.getByRole('button', { name: 'Nothing selected' }).click();
+  //  await page.getByRole('menu').locator('a').filter({ hasText: 'Yes - 30% Haircut to be' }).click();
+  if (foreignCurrency || foreignCurrency === false) {
+    await page.getByRole("button", { name: "Nothing selected" }).click();
+    await page.getByRole("menu").locator("a").filter({ hasText: "No" }).click();
+  }
+
+  if (additionalIncome || additionalIncome === 0) {
+    const additionalIncomeInput = page.locator(
+      `#a${applicantNumber}additionalIncome`
+    );
+    await clickAndEnterNumericalInput(additionalIncomeInput, additionalIncome);
+  }
+
+  if (limitedCompanyNetProfits || limitedCompanyNetProfits === 0) {
+    const limitedCompanyNetProfitsInput = page.locator(
+      `#a${applicantNumber}dividendIncome`
+    );
+    await clickAndEnterNumericalInput(
+      limitedCompanyNetProfitsInput,
+      limitedCompanyNetProfits
+    );
+  }
+  if (otherNonTaxableIncome || otherNonTaxableIncome === 0) {
+    const otherNonTaxableIncomeInput = page.locator(
+      `#a${applicantNumber}otherNonTaxableIncome`
+    );
+    await clickAndEnterNumericalInput(
+      otherNonTaxableIncomeInput,
+      otherNonTaxableIncome
+    );
+  }
+
+  if (existingBTLRentalIncome || existingBTLRentalIncome === 0) {
+    const existingBTLRentalIncomeInput = page.locator(
+      `#a${applicantNumber}existingBTLRentalIncome`
+    );
+    await clickAndEnterNumericalInput(
+      existingBTLRentalIncomeInput,
+      existingBTLRentalIncome
+    );
   }
 }
