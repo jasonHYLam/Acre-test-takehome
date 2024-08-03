abbebd0f9062baa3ed8d248391cb79460841416c
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-08-03 16:40:53 +0100

    Add id selector to selectOptions for handleExpenditureDetails

--- helpers/handleExpenditureDetails.ts
+++ helpers/handleExpenditureDetails.ts
@@ -46,47 +46,47 @@ async function handleExpenditureDetailsForOneApplicant(
 
   if (monthlyBTLOutgoings || monthlyBTLOutgoings === 0) {
     const monthlyBTLOutgoingsInput = page.locator(
-      `a${applicantNumber}existingBTLOutgoings`
+      `#a${applicantNumber}existingBTLOutgoings`
     );
     clickAndEnterNumericalInput(monthlyBTLOutgoingsInput, monthlyBTLOutgoings);
   }
 
   if (monthlyLoanPayments || monthlyLoanPayments === 0) {
     const monthlyLoanPaymentsInput = page.locator(
-      `a${applicantNumber}totalMonthlyLoanPayments`
+      `#a${applicantNumber}totalMonthlyLoanPayments`
     );
     clickAndEnterNumericalInput(monthlyLoanPaymentsInput, monthlyLoanPayments);
   }
 
   if (creditCards || creditCards === 0) {
     const creditCardsInput = page.locator(
-      `a${applicantNumber}outstandingCreditCardBalances`
+      `#a${applicantNumber}outstandingCreditCardBalances`
     );
     clickAndEnterNumericalInput(creditCardsInput, creditCards);
   }
 
   if (groundRent || groundRent === 0) {
     const groundRentInput = page.locator(
-      `a${applicantNumber}rentAndServiceCharge`
+      `#a${applicantNumber}rentAndServiceCharge`
     );
     clickAndEnterNumericalInput(groundRentInput, groundRent);
   }
 
   if (travel || travel === 0) {
-    const travelInput = page.locator(`a${applicantNumber}travel`);
+    const travelInput = page.locator(`#a${applicantNumber}travel`);
     clickAndEnterNumericalInput(travelInput, travel);
   }
 
   if (childCareCosts || childCareCosts === 0) {
     const childCareCostsInput = page.locator(
-      `a${applicantNumber}childcareCosts`
+      `#a${applicantNumber}childcareCosts`
     );
     clickAndEnterNumericalInput(childCareCostsInput, childCareCosts);
   }
 
   if (otherExpenditure || otherExpenditure === 0) {
     const otherExpenditureInput = page.locator(
-      `a${applicantNumber}otherExpenditure`
+      `#a${applicantNumber}otherExpenditure`
     );
     clickAndEnterNumericalInput(otherExpenditureInput, otherExpenditure);
   }
