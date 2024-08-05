7b0f819750f2b4044e84e2e312e43d52aaaf29e3
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-08-05 14:03:29 +0100

    Update README

--- README.md
+++ README.md
@@ -6,13 +6,26 @@ Takehome assignment for Acre test role.
 
 I have used the Playwright library for its ease of end-to-end testing, and wrote the tests specifically for the HSBC affordability residential calculator.
 
-I have written a table-driven test that handles multiple scenarios as directed by the assignment brief. The table-driven test approach uses a collection of input data, loops through it and performs the same test for each loop iteration. To handle different scenarios, there are many if statements within the test to create different branches of execution. However I would have otherwise written individual tests, as I find it is easier to test specific cases.
+I have written a table-driven test that handles multiple scenarios as directed by the assignment brief. The table-driven test approach uses a collection of test entries and loops through it to perform the same test for each loop iteration. These test entries contain input data, expected results and test names. To handle different scenarios, there are many if statements within the test to create different branches of execution.
+
+I have used the Page Object model to perform actions on various parts of the page such as clicking buttons and filling inputs, and obtaining values from the page (using locators) to compare to the expected results for assertions.
+
+I have introduced a number of conditional statements in the table test to handle different inputs and thus create the results for different scenarios. These scenarios include:
+
+- When minimum details are provided for lending and and the values are great enough for lending => lending
+- When the minimum details for lending are provided but the values are too low for lending => no lending
+- When minimum details are provided for lending, but the property value is too low => no lending
+- When minimum details are provided for lending, but income is too low compared to property value => no lending
+- When minimum details are provided for lending, but expenditure is too high compared to income => no lending
+- When no details are provided => no lending with results error
+
+The test then compares the actual results with the expected results provided, using the `expect` function.
 
 The input data is provided in the `inputData.ts` file. I believe this would suffice for a takehome assignment as I have interpreted the purpose of the assignment to focus more on the data's types and range rather than the way it is setup, however in a real-world scenario I might obtain the data from a database especially when performing read/write database operations.
 
 The input data can take all possible input from the HSBC affordability residential calculator form. Types are introduced to ensure that the structure of the data is consistent. Additionally, the use of union types ensure that certain input is restricted to expected values, which is useful for select input types such as Marital Status, Maximum LTV etc.
 
-I have observed that several of the inputs do not seem to affect the lending calculation, such as postcode. I have nonetheless included them in the allowed range of input as well as handling input actions.
+I have observed that several of the inputs do not seem to affect the lending calculation, such as postcode and marital status. I have nonetheless included them in the allowed range of input as well as handling input actions. I could have tested these inputs by checking the "Printer Friendly Version" and verifying that the those values are present, but I assumed the test only needed to focus on whether lending would be given or not.
 
 I have separated concerns by putting constants, types, and util helper functions in their respective files and importing them where necessary.
 
@@ -34,7 +47,7 @@ The test needs to handle cases where there is a single applicant or two applican
 
 I do not know the exact workings of the calculator and the relationship between income, expenditure and mortgage details. I wasn't sure how much expenditure leads to no lending with certain values of income and mortgage property value. That being said I attempted to model these relationships with simple ratios, such as `maximum expenditure to income ratio for lending`. I have attempted to write scenarios with different values for each category, such that lending is either given or not given.
 
-There were many edge cases due to the complex relationship between income, expenditure and properties. For instance, when expenditure is extremely low (£1) and income is at the minimum required for lending (£10000), that results in no lending despite being far lower than the `maximum expenditure to income ratio for lending`. I found capturing these all edge cases to be quite difficult with if blocks, thus I would have preferred to use individual tests rather than a table-driven test approach.
+There were many edge cases due to the complex relationship between income, expenditure and properties. For instance, when expenditure is extremely low (£1) and income is at the minimum required for lending (£10000), that results in no lending despite being far lower than the `maximum expenditure to income ratio for lending`.
 
 ## Build and Run
 
--- tests/testData/testData.ts
+++ tests/testData/testData.ts
@@ -26,173 +26,173 @@ export const testData: TestEntry[] = [
     },
   },
 
-  // {
-  //   testName:
-  //     "Scenario where lending is given, with additional mortgage details provided.",
-  //   providedDetails: {
-  //     mortgageDetails: {
-  //       propertyValue: 1000000,
-  //       jointMortgage: false,
-  //       maxLTV: 0.85,
-  //       mortgageTerm: 5,
-  //     },
-  //     allIncomeDetails: {
-  //       applicant1IncomeDetails: {
-  //         grossIncome: 100000,
-  //         foreignCurrency: false,
-  //       },
-  //     },
-  //   },
-  //   expectedResult: {
-  //     resultErrors: false,
-  //     lendingBasedOnPropertyError: "",
-  //     lendingBasedOnPropertyValue: 185950,
-  //     resultantLTV: 19,
-  //     lendingBasedOnAffordabilityError: "",
-  //     lendingBasedOnAffordabilityValue: 185950,
-  //   },
-  // },
+  {
+    testName:
+      "Scenario where lending is given, with additional mortgage details provided.",
+    providedDetails: {
+      mortgageDetails: {
+        propertyValue: 1000000,
+        jointMortgage: false,
+        maxLTV: 0.85,
+        mortgageTerm: 5,
+      },
+      allIncomeDetails: {
+        applicant1IncomeDetails: {
+          grossIncome: 100000,
+          foreignCurrency: false,
+        },
+      },
+    },
+    expectedResult: {
+      resultErrors: false,
+      lendingBasedOnPropertyError: "",
+      lendingBasedOnPropertyValue: 185950,
+      resultantLTV: 19,
+      lendingBasedOnAffordabilityError: "",
+      lendingBasedOnAffordabilityValue: 185950,
+    },
+  },
 
-  // {
-  //   testName:
-  //     "Scenario where lending is not given, as income is too low compared to property value.",
+  {
+    testName:
+      "Scenario where lending is not given, as income is too low compared to property value.",
 
-  //   providedDetails: {
-  //     mortgageDetails: { propertyValue: 1000000 },
-  //     allIncomeDetails: {
-  //       applicant1IncomeDetails: { grossIncome: 10000, foreignCurrency: false },
-  //     },
-  //   },
-  //   expectedResult: {
-  //     resultErrors: false,
-  //     lendingBasedOnPropertyError: "NOT AVAILABLE",
-  //     lendingBasedOnPropertyValue: 0,
-  //     resultantLTV: 0,
-  //     lendingBasedOnAffordabilityError: "NOT AVAILABLE",
-  //     lendingBasedOnAffordabilityValue: 0,
-  //   },
-  // },
+    providedDetails: {
+      mortgageDetails: { propertyValue: 1000000 },
+      allIncomeDetails: {
+        applicant1IncomeDetails: { grossIncome: 10000, foreignCurrency: false },
+      },
+    },
+    expectedResult: {
+      resultErrors: false,
+      lendingBasedOnPropertyError: "NOT AVAILABLE",
+      lendingBasedOnPropertyValue: 0,
+      resultantLTV: 0,
+      lendingBasedOnAffordabilityError: "NOT AVAILABLE",
+      lendingBasedOnAffordabilityValue: 0,
+    },
+  },
 
-  // {
-  //   testName:
-  //     "Scenario where lending is not given, as expenditure is too great compared to income.",
+  {
+    testName:
+      "Scenario where lending is not given, as expenditure is too great compared to income.",
 
-  //   providedDetails: {
-  //     mortgageDetails: { propertyValue: 1000000 },
-  //     allIncomeDetails: {
-  //       applicant1IncomeDetails: {
-  //         grossIncome: 100000,
-  //         foreignCurrency: false,
-  //       },
-  //     },
-  //     allExpenditureDetails: {
-  //       applicant1ExpenditureDetails: { monthlyBTLOutgoings: 3800 },
-  //     },
-  //   },
-  //   expectedResult: {
-  //     resultErrors: false,
-  //     lendingBasedOnPropertyError: "NOT AVAILABLE",
-  //     lendingBasedOnPropertyValue: 0,
-  //     resultantLTV: 0,
-  //     lendingBasedOnAffordabilityError: "NOT AVAILABLE",
-  //     lendingBasedOnAffordabilityValue: 0,
-  //   },
-  // },
+    providedDetails: {
+      mortgageDetails: { propertyValue: 1000000 },
+      allIncomeDetails: {
+        applicant1IncomeDetails: {
+          grossIncome: 100000,
+          foreignCurrency: false,
+        },
+      },
+      allExpenditureDetails: {
+        applicant1ExpenditureDetails: { monthlyBTLOutgoings: 3800 },
+      },
+    },
+    expectedResult: {
+      resultErrors: false,
+      lendingBasedOnPropertyError: "NOT AVAILABLE",
+      lendingBasedOnPropertyValue: 0,
+      resultantLTV: 0,
+      lendingBasedOnAffordabilityError: "NOT AVAILABLE",
+      lendingBasedOnAffordabilityValue: 0,
+    },
+  },
 
-  // {
-  //   testName:
-  //     "Scenario where lending is not given, as foreignCurrency is not provided.",
-  //   providedDetails: {
-  //     mortgageDetails: { propertyValue: 1000000 },
-  //     allIncomeDetails: {
-  //       applicant1IncomeDetails: { grossIncome: 100000 },
-  //     },
-  //   },
-  //   expectedResult: {
-  //     resultErrors: true,
-  //     lendingBasedOnPropertyError: "",
-  //     lendingBasedOnPropertyValue: 0,
-  //     resultantLTV: 0,
-  //     lendingBasedOnAffordabilityError: "NOT AVAILABLE",
-  //     lendingBasedOnAffordabilityValue: 0,
-  //   },
-  // },
+  {
+    testName:
+      "Scenario where lending is not given, as foreignCurrency is not provided.",
+    providedDetails: {
+      mortgageDetails: { propertyValue: 1000000 },
+      allIncomeDetails: {
+        applicant1IncomeDetails: { grossIncome: 100000 },
+      },
+    },
+    expectedResult: {
+      resultErrors: true,
+      lendingBasedOnPropertyError: "",
+      lendingBasedOnPropertyValue: 0,
+      resultantLTV: 0,
+      lendingBasedOnAffordabilityError: "NOT AVAILABLE",
+      lendingBasedOnAffordabilityValue: 0,
+    },
+  },
 
-  // {
-  //   testName: "Scenario where lending is not given, as propertyValue is 0.",
-  //   providedDetails: {
-  //     mortgageDetails: { propertyValue: 0 },
-  //     allIncomeDetails: {
-  //       applicant1IncomeDetails: {
-  //         grossIncome: 100000,
-  //         foreignCurrency: false,
-  //       },
-  //     },
-  //   },
-  //   expectedResult: {
-  //     resultErrors: true,
-  //     lendingBasedOnPropertyError: "",
-  //     lendingBasedOnPropertyValue: 0,
-  //     resultantLTV: 0,
-  //     lendingBasedOnAffordabilityError: "NOT AVAILABLE",
-  //     lendingBasedOnAffordabilityValue: 0,
-  //   },
-  // },
+  {
+    testName: "Scenario where lending is not given, as propertyValue is 0.",
+    providedDetails: {
+      mortgageDetails: { propertyValue: 0 },
+      allIncomeDetails: {
+        applicant1IncomeDetails: {
+          grossIncome: 100000,
+          foreignCurrency: false,
+        },
+      },
+    },
+    expectedResult: {
+      resultErrors: true,
+      lendingBasedOnPropertyError: "",
+      lendingBasedOnPropertyValue: 0,
+      resultantLTV: 0,
+      lendingBasedOnAffordabilityError: "NOT AVAILABLE",
+      lendingBasedOnAffordabilityValue: 0,
+    },
+  },
 
-  // {
-  //   testName:
-  //     "Scenario where lending is not given, as propertyValue is a negative integer.",
-  //   providedDetails: {
-  //     mortgageDetails: { propertyValue: -1 },
-  //     allIncomeDetails: {
-  //       applicant1IncomeDetails: {
-  //         grossIncome: 100000,
-  //         foreignCurrency: false,
-  //       },
-  //     },
-  //   },
-  //   expectedResult: {
-  //     resultErrors: true,
-  //     lendingBasedOnPropertyError: "",
-  //     lendingBasedOnPropertyValue: 0,
-  //     resultantLTV: 0,
-  //     lendingBasedOnAffordabilityError: "NOT AVAILABLE",
-  //     lendingBasedOnAffordabilityValue: 0,
-  //   },
-  // },
+  {
+    testName:
+      "Scenario where lending is not given, as propertyValue is a negative integer.",
+    providedDetails: {
+      mortgageDetails: { propertyValue: -1 },
+      allIncomeDetails: {
+        applicant1IncomeDetails: {
+          grossIncome: 100000,
+          foreignCurrency: false,
+        },
+      },
+    },
+    expectedResult: {
+      resultErrors: true,
+      lendingBasedOnPropertyError: "",
+      lendingBasedOnPropertyValue: 0,
+      resultantLTV: 0,
+      lendingBasedOnAffordabilityError: "NOT AVAILABLE",
+      lendingBasedOnAffordabilityValue: 0,
+    },
+  },
 
-  // {
-  //   testName:
-  //     "Scenario where lending is not given, as propertyValue is below minimum property value.",
-  //   providedDetails: {
-  //     mortgageDetails: { propertyValue: 1 },
-  //     allIncomeDetails: {
-  //       applicant1IncomeDetails: {
-  //         grossIncome: 100000,
-  //         foreignCurrency: false,
-  //       },
-  //     },
-  //   },
-  //   expectedResult: {
-  //     resultErrors: false,
-  //     lendingBasedOnPropertyError: "NOT AVAILABLE",
-  //     lendingBasedOnPropertyValue: 0,
-  //     resultantLTV: 0,
-  //     lendingBasedOnAffordabilityError: "NOT AVAILABLE",
-  //     lendingBasedOnAffordabilityValue: 0,
-  //   },
-  // },
+  {
+    testName:
+      "Scenario where lending is not given, as propertyValue is below minimum property value.",
+    providedDetails: {
+      mortgageDetails: { propertyValue: 1 },
+      allIncomeDetails: {
+        applicant1IncomeDetails: {
+          grossIncome: 100000,
+          foreignCurrency: false,
+        },
+      },
+    },
+    expectedResult: {
+      resultErrors: false,
+      lendingBasedOnPropertyError: "NOT AVAILABLE",
+      lendingBasedOnPropertyValue: 0,
+      resultantLTV: 0,
+      lendingBasedOnAffordabilityError: "NOT AVAILABLE",
+      lendingBasedOnAffordabilityValue: 0,
+    },
+  },
 
-  // {
-  //   testName: "Scenario where no details are provided.",
-  //   providedDetails: {},
-  //   expectedResult: {
-  //     resultErrors: true,
-  //     lendingBasedOnPropertyError: "NOT AVAILABLE",
-  //     lendingBasedOnPropertyValue: 0,
-  //     resultantLTV: 0,
-  //     lendingBasedOnAffordabilityError: "NOT AVAILABLE",
-  //     lendingBasedOnAffordabilityValue: 0,
-  //   },
-  // },
+  {
+    testName: "Scenario where no details are provided.",
+    providedDetails: {},
+    expectedResult: {
+      resultErrors: true,
+      lendingBasedOnPropertyError: "NOT AVAILABLE",
+      lendingBasedOnPropertyValue: 0,
+      resultantLTV: 0,
+      lendingBasedOnAffordabilityError: "NOT AVAILABLE",
+      lendingBasedOnAffordabilityValue: 0,
+    },
+  },
 ];
